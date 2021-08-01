47747abd7ad2c5b7f8d1c4593cae4c2f15067749
Sai Reddy <sai6221@gmail.com>
2021-08-01 23:28:35 +0530

    Tip and stepper done

--- Tipsy/Controller/ViewController.swift
+++ Tipsy/Controller/ViewController.swift
@@ -15,23 +15,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     
+    var tipPercent: Float = 0.0
+    
     override func viewDidLoad() {
+        splitNumberLabel.text = String(2)
         super.viewDidLoad()
         
     }
 
     @IBAction func tipChanged(_ sender: UIButton) {
         //update UI
+        updateUI()
         let button = sender.currentTitle!
         updateTip(i: button)
         sender.isSelected = true
+        tipPercent = Float(String(button.dropLast()))!/100
+        print(tipPercent)
     }
 
 
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
+        print(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+//        print(tipPercent)
+        updateUI()
     }
     
     func updateTip(i: String){
@@ -51,6 +61,14 @@ class ViewController: UIViewController {
             tenPctButton.isSelected = false
             twentyPctButton.backgroundColor = #colorLiteral(red: 0.4500938654, green: 0.9813225865, blue: 0.4743030667, alpha: 0.6115434659)
         }
+    }
+    func updateUI(){
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        zeroPctButton.backgroundColor = UIColor.clear
+        tenPctButton.backgroundColor = UIColor.clear
+        twentyPctButton.backgroundColor = UIColor.clear
     }
 }
 
